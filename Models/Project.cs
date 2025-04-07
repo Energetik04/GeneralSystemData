a7d3ba91922c8e696418a3698d04ae31145b0374
Energetik04 <deathxy@yandex.by>
2025-04-07 16:15:34 +0300

    First Models

---
+++ Models/Project.cs
@@ -0,0 +1,8 @@
+public class Project
+{
+    public int Id { get; set; }
+    public DateTime DateOfConclusion{ get; set; }
+    public int NumberConclusion{ get; set; }
+    public string Name { get; set; }
+    
+}
---
+++ Models/Worker.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+public class Worker
+{
+    public int Id{ get; set; }
+    public string Name { get; set; }
+    public string Surname{ get; set; }
+    public List<Project> Projects { get; set; }
+    
+}
